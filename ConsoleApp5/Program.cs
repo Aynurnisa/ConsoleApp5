a473b2c256e83ecb6c55844fcc5681830d9e1d7c
Aynurnisa Özer <Aynur_Nisa@outlook.dk>
2019-05-09 12:01:46 +0200

    d

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -11,8 +11,8 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Dette er Branch 2");
-            Console.WriteLine("");
+            Console.WriteLine("Dette  er Branch 2");
+            Console.WriteLine(" ");
 
             Console.ReadKey();
         }
