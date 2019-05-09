1b161e30925941b17d2316061a01d0361843a933
Daniel K <turgandeminos@gmail.com>
2019-05-09 11:11:30 +0200

    Person v.1

---
+++ ConsoleApp5/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Person
+    {
+        private string navn;
+        private int alder;
+
+        public Person(string n, int a)
+        {
+            navn = n;
+            alder = a;
+        }
+
+    }
+}
